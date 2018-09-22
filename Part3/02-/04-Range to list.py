8e1d1781c686c64d7ad7506a0f60449dcda5bc63
muneeral <CodingEngcpp@gmail.com>
2018-09-21 17:13:47 -0700

    updating and adding new file

---
+++ Part3/02-/04-Range to list.py
@@ -0,0 +1,18 @@
+
+def range_to_list(k):
+    result = []
+    for i in range(0, k):
+        result.append(str(i))
+    return result
+
+
+def base_k_strings(n, k):
+    if n == 0:
+        return []
+    if n == 1:
+        return range_to_list(k)
+    return [digit + bitstring for digit in base_k_strings(1, k) for bitstring in base_k_strings(n - 1, k)]
+
+
+print(base_k_strings(2, 3))
+# print(range_to_list(5))
