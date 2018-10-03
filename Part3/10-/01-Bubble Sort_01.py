20fda1fb9854a2dfc079188bd98da4db2052355b
Muneer <CodingEngcpp@gmail.com>
2018-10-02 17:31:39 -0700

    Adding bubble sort example

--- Part3/10-/01-Bubble Sort_01.py
+++ Part3/10-/01-Bubble Sort_01.py
@@ -1,18 +1,22 @@
 
-# def bubblesort(A):
-#     for i in range(len(A)):
-#         if (A[i]>A[i+1]):
-            
+def bubblesort(A):
+    for i in range(len(A)):
+        for k in range(len(A)-1, i, -1):
+            if (A[k] < A[k-1]):
+                swap(A, k, k-1)
 
 
-
-# def main():
-#     mylist = [452, 677, 123, 452, 694, 42, 544, 134, 587, 900]
-#     bubblesort(A)
-#     print(A)
-
-
-# if __name__ == '__main__':
-#     main()
+def swap(A, x, y):
+    temp = A[x]
+    A[x] = A[y]
+    A[y] = temp
 
 
+def main():
+    mylist = [452, 677, 123, 452, 694, 42, 544, 134, 587, 900]
+    bubblesort(mylist)
+    print(mylist)
+
+
+if __name__ == '__main__':
+    main()
