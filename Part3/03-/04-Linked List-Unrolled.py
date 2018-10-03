660ee578cd1f64409ada55d402d030ba8dd972c9
Muneer <CodingEngcpp@gmail.com>
2018-10-03 15:13:43 -0700

    Adding new file and file name update

---
+++ Part3/03-/04-Linked List-Unrolled.py
@@ -0,0 +1,45 @@
+#
+#
+#
+
+block_size = 2
+block_head = None
+
+
+class Node:
+    def __init__(self):
+        self.value = None
+        self.next = None
+
+
+class LinkedBlock:
+    def __init__(self):
+        self.head = None
+        self.next = None
+        self.node_count = 0
+
+
+def new_node():
+
+
+def new_link_block():
+    block = LinkedBlock()
+    
+
+def add_element(block, value):
+    global block_head
+    r = new_link_block()
+
+def search_element():
+
+
+def shift():
+
+
+def main():
+    print('This is main function')
+    add_element(0, 10)
+
+
+if __name__ == '__main__':
+    main()
